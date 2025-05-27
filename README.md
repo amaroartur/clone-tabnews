190915bed84e01cfd76dca0db3d84746af691e40
Artur Amaro <amaroartur2@gmail.com>
2025-05-27 14:58:27 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para estudos onde ocorrerá a implementação do https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100dvh' }}>
-      <h1 style={{ textAlign: 'center' }}>Estava estudando aqui e lembrei de você</h1>
-      <h1 style={{ textAlign: 'center' }}>Tô com saudades 💙</h1>
-      <h1 style={{ textAlign: 'center' }}><strong>PS: </strong>quero outro(s) salgado(s) de queijo</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100dvh",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        Estava estudando aqui e lembrei de você
+      </h1>
+      <h1 style={{ textAlign: "center" }}>Tô com saudades 💙</h1>
+      <h1 style={{ textAlign: "center" }}>
+        <strong>PS: </strong>quero outro(s) salgado(s) de queijo
+      </h1>
     </div>
-  )
+  );
 }
